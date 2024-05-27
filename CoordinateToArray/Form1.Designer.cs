28d87855b56382ab5e88da412d4b8df08ad032a1
BaroneGiallo <96001978+BaroneGiallo@users.noreply.github.com>
2024-05-27 15:13:48 +0200

    Add files via upload

---
+++ CoordinateToArray/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿using System.Windows.Forms;
+
+namespace CoordinateToArray
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.ImportToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.SavetoolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ClearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(774, 381);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ImportToolStripMenuItem,
+            this.SavetoolStripMenuItem,
+            this.ClearToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(799, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // ImportToolStripMenuItem
+            // 
+            this.ImportToolStripMenuItem.Name = "ImportToolStripMenuItem";
+            this.ImportToolStripMenuItem.Size = new System.Drawing.Size(91, 20);
+            this.ImportToolStripMenuItem.Text = "Import image";
+            this.ImportToolStripMenuItem.Click += new System.EventHandler(this.ImportToolStripMenuItem_Click);
+            // 
+            // SavetoolStripMenuItem
+            // 
+            this.SavetoolStripMenuItem.Name = "SavetoolStripMenuItem";
+            this.SavetoolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.SavetoolStripMenuItem.Text = "Save";
+            this.SavetoolStripMenuItem.Click += new System.EventHandler(this.SaveToolStripMenuItem_Click);
+            // 
+            // ClearToolStripMenuItem
+            // 
+            this.ClearToolStripMenuItem.Name = "ClearToolStripMenuItem";
+            this.ClearToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.ClearToolStripMenuItem.Text = "Clear";
+            this.ClearToolStripMenuItem.Click += new System.EventHandler(this.ClearToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(799, 421);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "From Image to Array of Coordinates";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem ImportToolStripMenuItem;
+        private ToolStripMenuItem SavetoolStripMenuItem;
+        private ToolStripMenuItem ClearToolStripMenuItem;
+    }
+}
+
---
+++ CoordinateToArray/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace CoordinateToArray
+{
+    public partial class Form1 : Form
+    {
+        private List<Point> points = new List<Point>();
+        private Point? startPoint = null;
+        private Point? endPoint = null;
+        private bool drawing = false;
+        private bool immagine = false;
+        private Point previousMousePosition;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.MouseWheel += Form1_MouseWheel;
+            this.MouseDown += Form1_MouseDown;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+        }
+
+        private void Form1_MouseWheel(object sender, MouseEventArgs e)
+        {
+            if (pictureBox1.Image != null && immagine)
+            {
+                int delta = e.Delta;
+                float scaleFactor = delta > 0 ? 1.1f : 0.9f;
+                pictureBox1.Width = (int)(pictureBox1.Width * scaleFactor);
+                pictureBox1.Height = (int)(pictureBox1.Height * scaleFactor);
+                pictureBox1.Refresh();
+            }
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Middle && immagine)
+            {
+                previousMousePosition = e.Location;
+            }
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (immagine)
+            {
+                if (e.Button == MouseButtons.Left)
+                {
+                    if (!drawing)
+                    {
+                        startPoint = e.Location;
+                        endPoint = null;
+                        points.Add(startPoint.Value);
+                        drawing = true;
+                    }
+                    else
+                    {
+                        endPoint = e.Location;
+                        DrawPoint(endPoint.Value);
+                        DrawLine();
+                        GeneratePointsOnLine();
+                        startPoint = endPoint;
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Inserici prima l'immagine!");
+            }
+        }
+
+        private void DrawPoint(Point point)
+        {
+            using (Graphics g = Graphics.FromImage(pictureBox1.Image))
+            {
+                g.FillEllipse(Brushes.Red, point.X - 3, point.Y - 3, 6, 6);
+            }
+            pictureBox1.Refresh();
+        }
+
+        private void DrawLine()
+        {
+            using (Graphics g = Graphics.FromImage(pictureBox1.Image))
+            {
+                g.DrawLine(Pens.Blue, startPoint.Value, endPoint.Value);
+            }
+            pictureBox1.Refresh();
+        }
+
+        private void GeneratePointsOnLine()
+        {
+            int dx = endPoint.Value.X - startPoint.Value.X;
+            int dy = endPoint.Value.Y - startPoint.Value.Y;
+            int steps = Math.Max(Math.Abs(dx), Math.Abs(dy));
+
+            float xIncrement = (float)dx / steps;
+            float yIncrement = (float)dy / steps;
+
+            float x = startPoint.Value.X;
+            float y = startPoint.Value.Y;
+
+            for (int i = 1; i <= steps; i++)
+            {
+                points.Add(new Point((int)Math.Round(x), (int)Math.Round(y)));
+                x += xIncrement;
+                y += yIncrement;
+            }
+        }
+
+        private void ImportToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files (*.png;*.jpg;*.jpeg;*.gif;*.bmp)|*.png;*.jpg;*.jpeg;*.gif;*.bmp|All files (*.*)|*.*";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    immagine = true;
+                    pictureBox1.Image = new Bitmap(openFileDialog.FileName);
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Si è verificato un errore durante il caricamento dell'immagine: " + ex.Message);
+                }
+            }
+        }
+
+        private void SaveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text Files (*.txt)|*.txt";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamWriter writer = new StreamWriter(saveFileDialog.FileName))
+                {
+                    writer.Write("[");
+                    for (int i = 0; i < points.Count; i++)
+                    {
+                        writer.Write($"[{points[i].X}, {points[i].Y}]");
+                        if (i < points.Count - 1)
+                        {
+                            writer.Write(", ");
+                        }
+                    }
+                    writer.Write("],");
+                }
+                MessageBox.Show("Coordinate salvate con successo!");
+            }
+        }
+
+        private void ClearToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = null;
+            immagine = false;
+        }
+    }
+}
